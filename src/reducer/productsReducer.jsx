2eaf69b09622140700eb0dc3b57da05f9d16cf70
thazin naing <thazinaingmm@gmail.com>
2023-08-19 10:44:04 +0630

    project setup

---
+++ src/reducer/productsReducer.jsx
@@ -0,0 +1,12 @@
+const initialState = {
+    products: [],
+}
+
+export const ProductsReducer= (state=initialState, action)=>{
+
+    switch(action.type){
+        case fetchProducts:{
+            
+        }
+    }
+}
